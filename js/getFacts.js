8661918d42f6275e490ccbccb78261ae9f52ce34
Guilherme A Romao <g.amoroso.r@gmail.com>
2021-04-06 10:43:16 +0100

    Added jQuery functionality for Ajax and elements

--- js/getFacts.js
+++ js/getFacts.js
@@ -1,38 +1,26 @@
-var ajax;
-
-if(window.XMLHttpRequest){
-    ajax = new XMLHttpRequest();
-}
-else if (window.ActiveXObject){
-    ajax = new ActiveXObject('MICROSOFT.XMLHTTP');
-}
-
-ajax.onreadystatechange = function() {
-    console.log("Received state change: " + ajax.readyState);
-    if(ajax.readyState == 4 && ajax.status == 200){
-        console.log("Received readyState 4");
-        var response = ajax.responseText;
-        var catFacts = JSON.parse(response);
-        console.log(catFacts);
-        fillFacts(catFacts);
+$.ajax({
+    url: "https://cat-fact.herokuapp.com/facts",
+    type: 'GET',
+    dataType: 'json', // added data type
+    success: function(res) {
+        console.log(res);
+        fillFacts(res);
     }
-}
+});
 
-var table = document.getElementById("table_cats");
+var table = $("#table_cats");
 var fillFacts = function(facts){
 
     facts.forEach(function(fact){
-        var row = table.insertRow(-1);
 
-        row.innerHTML = "<td>" + fact.text + "</td>";
-    })
+        var element = "<tr><td>" + fact.text + "</td></tr>";
+
+        $(element).appendTo(table);
+        
+    });
 }
 
-ajax.open("GET", "https://cat-fact.herokuapp.com/facts");
-ajax.setRequestHeader('Content-type', 'application/json');
-ajax.send();
-
-var date = document.getElementById("date");
+var date = $("#date");
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 var today = new Date();
